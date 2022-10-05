cd5279cba87ef020378a73c28056eafdfd18b4d9
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-10-05 14:01:20 +0200

    1. Separate out MaterialApp Widget into a StatelessWidget's build method & Use 'Hot Reload' while Updating the Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
