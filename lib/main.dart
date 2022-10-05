863c1870938626d6bc343901d3efbf1e06e65a6c
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-10-05 22:54:59 +0200

    7. Use Card & ListTile Widgets (How to use?)

--- lib/main.dart
+++ lib/main.dart
@@ -11,81 +11,75 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const CircleAvatar(
-                  radius: 50,
-                  foregroundImage: AssetImage('assets/images/fady.jpg'),
-                ),
-                const Text(
-                  'Eng.Fady Fawzy',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Pacifico',
-                  ),
-                ),
-                Text(
-                  'Software Engineer',
-                  style: TextStyle(
-                    color: Colors.teal[100],
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2.5,
-                    fontFamily: 'Source Sans Pro',
-                  ),
-                ),
-                const SizedBox(height: 16),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 32),
-                  padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 48,
+                foregroundImage: AssetImage('assets/images/fady.jpg'),
+              ),
+              const Text(
+                'Eng.Fady Fawzy',
+                style: TextStyle(
                   color: Colors.white,
-                  child: Row(
-                    children: [
-                      const Icon(
-                        Icons.phone_rounded,
-                        color: Colors.teal,
-                      ),
-                      const SizedBox(width: 16),
-                      Text(
-                        '+2 012 878 111 86',
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontSize: 18,
-                          fontFamily: 'Source Sans Pro',
-                        ),
-                      ),
-                    ],
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'Software Engineer',
+                style: TextStyle(
+                  color: Colors.teal[100],
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                  fontFamily: 'Source Sans Pro',
+                ),
+              ),
+              SizedBox(
+                height: 32,
+                width: 200,
+                child: Divider(color: Colors.teal[100]),
+              ),
+              Card(
+                margin: const EdgeInsets.symmetric(horizontal: 16),
+                child: ListTile(
+                  horizontalTitleGap: 8,
+                  leading: const Icon(
+                    Icons.phone_rounded,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+2 012 878 111 86',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 18,
+                      fontFamily: 'Source Sans Pro',
+                    ),
                   ),
                 ),
-                const SizedBox(height: 16),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 32),
-                  padding: const EdgeInsets.all(16),
-                  color: Colors.white,
-                  child: Row(
-                    children: [
-                      const Icon(
-                        Icons.email_rounded,
-                        color: Colors.teal,
-                      ),
-                      const SizedBox(width: 16),
-                      Text(
-                        'FadyFawzy.Official@gmail.com',
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontSize: 16,
-                          fontFamily: 'Source Sans Pro',
-                        ),
-                      ),
-                    ],
+              ),
+              const SizedBox(height: 16),
+              Card(
+                margin: const EdgeInsets.symmetric(horizontal: 16),
+                child: ListTile(
+                  horizontalTitleGap: 8,
+                  leading: const Icon(
+                    Icons.email_rounded,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'FadyFawzy.Official@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 18,
+                      fontFamily: 'Source Sans Pro',
+                    ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
