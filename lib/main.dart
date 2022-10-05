0d11edb0384c52a637913fb46952e9ef453a6479
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-10-05 22:29:13 +0200

    6. Add Contact Information using Icon Widget (The Icon widget)

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,12 @@ class MyApp extends StatelessWidget {
           child: Center(
             child: Column(
               mainAxisSize: MainAxisSize.min,
-              children: const [
-                CircleAvatar(
+              children: [
+                const CircleAvatar(
                   radius: 50,
                   foregroundImage: AssetImage('assets/images/fady.jpg'),
                 ),
-                Text(
+                const Text(
                   'Eng.Fady Fawzy',
                   style: TextStyle(
                     color: Colors.white,
@@ -31,11 +31,57 @@ class MyApp extends StatelessWidget {
                 Text(
                   'Software Engineer',
                   style: TextStyle(
-                    color: Color(0xFFB2DFDB),
+                    color: Colors.teal[100],
                     fontSize: 18,
                     fontWeight: FontWeight.bold,
                     letterSpacing: 2.5,
                     fontFamily: 'Source Sans Pro',
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 32),
+                  padding: const EdgeInsets.all(16),
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.phone_rounded,
+                        color: Colors.teal,
+                      ),
+                      const SizedBox(width: 16),
+                      Text(
+                        '+2 012 878 111 86',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 18,
+                          fontFamily: 'Source Sans Pro',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 32),
+                  padding: const EdgeInsets.all(16),
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.email_rounded,
+                        color: Colors.teal,
+                      ),
+                      const SizedBox(width: 16),
+                      Text(
+                        'FadyFawzy.Official@gmail.com',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontSize: 16,
+                          fontFamily: 'Source Sans Pro',
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
